5c8f9b998ea27a613d2bcdcac0e026cd8fe61c18
EmeryAtRTC <102991550+EmeryAtRTC@users.noreply.github.com>
2023-04-10 12:20:04 -0700

    added starting assignment

---
+++ Lab2/Models/Album.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab2.Models
+{
+    public class Album
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
